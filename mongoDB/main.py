d75fdd1c5e17c368275153bb1f8a83e909fea862
Yousuf Qutubuddin <usuf52@gmail.com>
2017-04-15 13:45:53 +0500

    modified post API

--- mongoDB/main.py
+++ mongoDB/main.py
@@ -8,42 +8,49 @@ app.config['MONGO_URI'] = 'mongodb://test:test@ds161400.mlab.com:61400/python-db
 
 mongo = PyMongo(app)
 
+
 @app.route('/', methods=['GET'])
 def index():
-  return 'Please run route /star or /star/Siris'
+    return 'Please run route /star or /star/Siris'
+
 
 @app.route('/star', methods=['GET'])
 def get_all_stars():
-  star = mongo.db.stars
-  output = []
-  for s in star.find():
-    output.append({'name' : s['name'], 'distance' : s['distance']})
-  return jsonify({'result' : output})
+    star = mongo.db.stars
+    output = []
+    for s in star.find():
+        output.append({'name': s['name'], 'distance': s['distance']})
+    return jsonify({'result': output})
+
 
 @app.route('/star/<name>', methods=['GET'])
 def get_one_star(name):
-  star = mongo.db.stars
-  s = star.find_one({'name' : name})
-  if s:
-    output = {'name' : s['name'], 'distance' : s['distance']}
-  else:
-    output = "No such name"
-  return jsonify({'result' : output})
+    star = mongo.db.stars
+    s = star.find_one({'name': name})
+    if s:
+        output = {'name': s['name'], 'distance': s['distance']}
+    else:
+        output = "No such name"
+    return jsonify({'result': output})
+
 
 @app.route('/star', methods=['POST'])
 def add_star():
-  star = mongo.db.stars
-  name = request.form.get('name')
-  distance = request.form.get('distance')
-  star_id = star.insert({'name': name, 'distance': distance})
-  new_star = star.find_one({'_id': star_id })
-  output = {'name' : new_star['name'], 'distance' : new_star['distance']}
-  return jsonify({'result' : output})
+    star = mongo.db.stars
+    obj = request.get_json(force=True)
+    name = obj['name']
+    distance = obj['distance']
+    star_id = star.insert(obj)
+    # name = request.form.get('name')
+    # distance = request.form.get('distance')
+    # star_id = star.insert({'name': name, 'distance': distance})
+    new_star = star.find_one({'_id': star_id})
+    output = {'name': new_star['name'], 'distance': new_star['distance']}
+    return jsonify({'result': output})
+    # return jsonify(request.get_json(force=True)), 201
 
 
-
-
-## readme
+# readme
 # For URL Query parameter, use request.args
 #search = request.args.get("search")
 #page = request.args.get("page")
@@ -52,11 +59,10 @@ def add_star():
 #email = request.form.get('email')
 #password = request.form.get('password')
 
-#For data type application/json, use request.data
+# For data type application/json, use request.data
 # data in string format and you have to parse into dictionary
 #data = request.data
 #dataDict = json.loads(data)
-
 
 
 if __name__ == '__main__':
